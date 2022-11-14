1cb6de4ebd8d0a63d3ee38aebe36a262d9ffe8c0
ysm19979 <19979@sangfor.com>
2022-11-14 21:45:45 +0800

    [feat] add the ssh files.
    include:
        1. ssh-keygen
        2. ssh-copy-id
        3. scp

--- core/pre_install/main.sh
+++ core/pre_install/main.sh
@@ -1,3 +1,19 @@
-# !/bin/bash
+#!/bin/bash
 
 yum install expect openssl openssh-server openssh-clients -y
+
+password=${1}
+nodes=${@:1}
+path="/var/ysm"
+
+./ssh-keygen.exp
+
+for node in ${nodes}; do
+    ./ssh-copy-id.exp "${node}" "$password"
+done
+
+for node in ${nodes}; do
+    ssh StrictHostKeyChecking=no "${node}" mkdir ${path}
+    scp StrictHostKeyChecking=no /var/ysm/* "${node}:${path}"
+    # ./scp.exp "${node}:/var/ysm"
+done
