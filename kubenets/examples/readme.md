dda2f30c247f4257ec7953f59c34716e7def1f5d
rootlulu <rootlulu@163.com>
2023-03-17 16:18:18 +0800

    [feat] optimize the project arch.

---
+++ kubenets/examples/readme.md
@@ -0,0 +1,5 @@
+# example introduce.
+
+## for successfully executing the examples, some rules.
+1. the pod example:
+    
--- post.sh
+++ post.sh
@@ -13,10 +13,12 @@ function post_main() {
 }
 
 function successInstalled() {
-    lcoal svc_ip
+    local svc_ip
     kubectl create deployment nginx --image=nginx:1.14-alpine
     kubectl expose deployment nginx --port=80 --type=NodePort
     svc_ip=$(kubectl get svc | awk '{if ($1 == "nginx") print $3}')
+    # to wait the service started.
+    sleep 10
     if [[ $(curl -s "${svc_ip}" -o /dev/null -w "%{http_code}") -eq 200 ]]; then
         kubectl delete svc,deploy nginx
         return 0
